f3b723656fca0ed09abeeb74a8ad0544f70b42f8
Zeber0 <56185363+Zeber0@users.noreply.github.com>
2021-10-27 05:37:46 +0300

    Add files via upload

--- lab3py.py
+++ lab3py.py
@@ -2,26 +2,34 @@ import requests
 
 
 class RequestsApi:
+    query = {}
     def __init__(self, base_url):
         self.base_url = base_url
         self.session = requests.Session()
+        self.updq()
+    def updq(self,q={"user-agent":"test_client"}):
+        self.query=q
+    def get(self, url, **kwargs):
+        return self.session.get(self.base_url + url,headers=self.query, **kwargs)
 
-    def get(self, url):
-        return self.session.get(self.base_url + url)
+    def head(self, url, **kwargs):
+        return self.session.head(self.base_url + url, **kwargs)
 
-    def head(self, url):
-        return self.session.head(self.base_url + url)
+    def post(self, url, **kwargs):
+        return self.session.post(self.base_url + url, headers=self.query, **kwargs)
 
-    def post(self, url):
-        return self.session.post(self.base_url + url)
-
-    def put(self, url):
-        return self.session.put(self.base_url + url)
-
+    def put(self, url, **kwargs):
+        return self.session.put(self.base_url + url, **kwargs)
     def __del__(self):
         self.session.close()
 
+a = RequestsApi("https://httpbin.org")
+response_3=a.get("/get", params={"key_1":"value_1"})
+print (response_3.text)
+a.updq({"user-agent": "test"})
+response_1 = a.get("/get", params={"key_1":"value_1"})
+print (response_1.text)
+a.updq()
+response = a.post("/post", data={"key_2":"value_2"})
+print (response.text)
 
-a = RequestsApi("https://api.github.com")
-b = a.get("/users/Zebr0")
-print(b.json())
